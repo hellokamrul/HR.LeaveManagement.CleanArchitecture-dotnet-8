fe061626fc63b21ff406847f8e0c1dccc837ff9c
unknown <nijumkamrul447@gmail.com>
2024-09-02 10:31:42 +0600

    initial

---
+++ HR.LeaveManagement.Application/DTOs/LeaveAllocation/CreateLeaveAllocationDto.cs
@@ -0,0 +1,14 @@
+﻿using HR.LeaveManagement.Application.DTOs.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.DTOs.LeaveAllocation
+{
+    public class CreateLeaveAllocationDto :BaseDto
+    {
+        public int LeaveTypeId { get; set; }
+    }
+}
